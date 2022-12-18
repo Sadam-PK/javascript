e1d41355ad7980ebd9af3df11e4220735c1ced02
sadam <sadamccr@gmail.com>
2022-12-18 13:46:33 +0500

    foreach done

--- javascript.js
+++ javascript.js
@@ -1,15 +1,73 @@
-sayHello =(name, greeting)=> {console.log('hi' + name + greeting)}
+// //------------ arrow functions --------------
+
+// sayHello =(name, greeting)=> {console.log('hi' + name + greeting)}
 
 
-sayHello('sadam', 'morning')  
+// sayHello('sadam', 'morning')  
 
 
-const x ={
-    name:'sadam',
-    age:22,
-    show:function(){
-        console.log(this.name)
-    }
-}
+// const x ={
+//     name:'sadam',
+//     age:22,
+//     show:function(){
+//         console.log(this.name)
+//     }
+// }
 
-x.show()
+// x.show()
+
+// //------------- foreach function ------------------
+
+// friends = ['shoaib', 'bilal', 'mukhlis']
+
+
+// friends.forEach(element=>{
+//     console.log(element)
+// })
+
+// for (element in friends){
+//     console.log(friends[element])
+// }
+
+// for(element of friends){
+//     console.log(element)
+// }
+
+// let employee={
+//     name:'Ali',
+//     age : 22
+// }
+
+// for (element in employee){
+//     console.log(`${element} = ${employee[element]}`)
+// }
+
+// list = [
+//     {
+//         name : 'asd',
+//         age: 22,
+//     },
+//     {
+//         name : 'ali',
+//         age: 12,
+//     },
+//     {
+//         name : 'sk',
+//         age: 33,
+//     }
+// ]
+
+// list.forEach(element => {
+//     console.log(element)
+// });
+
+// for(element of list){
+//     console.log(element)
+// }
+
+// for(element in list){
+//     console.log(list[element])
+// }
+
+
+// //-------- event handling ------------ 
